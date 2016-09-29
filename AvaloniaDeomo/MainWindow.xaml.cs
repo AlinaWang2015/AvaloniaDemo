780f8f20ba2193b055013671aef4679fe7c5d653
Alilna Wang <wangpengxia2015@outlook.com>
2016-09-29 16:49:50 +0800

    Add project files.

---
+++ AvaloniaDeomo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+
+namespace AvaloniaDeomo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            // TODO: iOS does not support dynamically loading assemblies
+            // so we must refer to this resource DLL statically. For
+            // now I am doing that here. But we need a better solution!!
+            var theme = new Avalonia.Themes.Default.DefaultTheme();
+            theme.FindResource("Button");
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Demo.Destop/Program.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Logging.Serilog;
+using Avalonia.Platform;
+using AvaloniaDeomo;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Destop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeLogging();
+
+            // TODO: Make this work with GTK/Skia/Cairo depending on command-line args
+            // again.
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+
+        // This will be made into a runtime configuration extension soon!
+        private static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
+        }
+
+        private static void ConfigureAssetAssembly(AppBuilder builder)
+        {
+            AvaloniaLocator.CurrentMutable
+                .GetService<IAssetLoader>()
+                .SetDefaultAssembly(typeof(App).Assembly);
+        }
+    }
+}
